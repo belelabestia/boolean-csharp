1fc14de82f3a2754440acb18d22cbdec36ef9eb8
Marco Bellezza <jasone.mb@outlook.com>
2023-03-21 10:10:26 +0100

    add shop ex.

---
+++ Shop/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+public record Product
+{
+    string name = "";
+    string description = "";
+
+    public Product() : this(name: "") { }
+    public Product(string name) : this(code: new Random().Next(), name: name) { }
+
+    public Product(int code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+
+    public int Code { get; private init; }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+
+    public string Description
+    {
+        get => description;
+        set => description = value;
+    }
+
+    public double Price { get; set; }
+    public double Iva { get; set; }
+
+    public double GetBasePrice() => Price;
+    public double GetPriceAfterIva() => Price * (1 + Iva);
+    public string GetExtendedName() => Code.ToString().PadLeft(20, '0') + " " + Name;
+}
---
+++ Shop/Program.cs
@@ -0,0 +1,9 @@
+﻿var p = new Product("Libro")
+{
+    Price = 100,
+    Iva = 0.22,
+    Description = "Un bel libro da leggere.",
+};
+
+Console.WriteLine(p);
+Console.WriteLine($"price after iva: {p.GetPriceAfterIva()}; extended name: {p.GetExtendedName()}.");
