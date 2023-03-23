8665e19e604ec02fb4b88ada13909b89e880d932
Marco Bellezza <jasone.mb@outlook.com>
2023-03-23 10:32:16 +0100

    add calcoli helper ex.

---
+++ CalcoliHelper/CalcoliHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Somma di due numeri interi
+Somma di due numeri double
+Differenza tra due numeri interi
+Differenza tra due numeri double
+Moltiplicazione di due numeri interi
+Moltiplicazione di due numeri double
+Valore assoluto di un numero intero
+Valore assoluto di un numero double
+Minimo tra due numeri interi
+Minimo tra due numeri double
+Massimo tra due numeri interi
+Massimo tra due numeri double
+ */
+
+public static class CalcoliHelper
+{
+    public static int Somma(int primo, int secondo) => primo + secondo;
+    public static double Somma(double primo, double secondo) => primo + secondo;
+    public static int Sottrai(int primo, int secondo) => primo - secondo;
+    public static double Sottrai(double primo, double secondo) => primo - secondo;
+    public static int Moltiplica(int primo, int secondo) => primo * secondo;
+    public static double Moltiplica(double primo, double secondo) => primo * secondo;
+    public static int ValoreAssoluto(int num) => num > 0 ? num : -num;
+    public static double ValoreAssoluto(double num) => num > 0 ? num : -num;
+    public static int Minimo(int primo, int secondo) => primo < secondo ? primo : secondo;
+    public static double Minimo(double primo, double secondo) => primo < secondo ? primo : secondo;
+    public static int Massimo(int primo, int secondo) => primo > secondo ? primo : secondo;
+    public static double Massimo(double primo, double secondo) => primo > secondo ? primo : secondo;
+
+    public static double? Potenza(double @base, double esponente)
+    {
+        if (esponente == 0) return 1;
+        if (esponente < 0 && @base == 0) return null;
+        if (@base == 0) return 0;
+        if (@base == 1) return 1;
+
+        double risultato = @base;
+        
+        for (int i = 1; i < ValoreAssoluto(esponente); i++)
+        {
+            risultato *= @base;
+        }
+
+        return esponente > 0 ? risultato : 1 / risultato;
+    }
+}
---
+++ CalcoliHelper/Program.cs
@@ -0,0 +1,23 @@
+﻿using static CalcoliHelper;
+using static System.Console;
+
+//WriteLine(ValoreAssoluto(2));
+//WriteLine(ValoreAssoluto(-1));
+
+//WriteLine(ValoreAssoluto(1.2));
+//WriteLine(ValoreAssoluto(-2.45));
+
+//WriteLine(Minimo(43, 56));
+//WriteLine(Minimo(43.493, 56.4879));
+
+//WriteLine(Massimo(43, 56));
+//WriteLine(Massimo(43.493, 56.4879));
+
+//WriteLine(Potenza(1, 87));
+//WriteLine(Potenza(0, 0));
+//WriteLine(Potenza(0, 88));
+//WriteLine(Potenza(0, -2));
+
+WriteLine(Potenza(-2, -2));
+WriteLine(Potenza(-2, 4));
+WriteLine(Potenza(-2, 3));
