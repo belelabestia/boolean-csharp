86c3300da48e82c4161d1890ac0ad0aeb8621810
Marco Bellezza <jasone.mb@outlook.com>
2023-03-27 10:06:31 +0200

    add lista indirizzi ex.

---
+++ ListaIndirizzi/Parser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    public static class Parser
+    {
+        public const string InputFile = "..\\..\\..\\addresses.csv";
+
+        public static IEnumerable<Address> Read()
+        {
+            using var input = File.OpenText(InputFile);
+            var addresses = new List<Address>();
+
+            input.ReadLine();
+
+            Console.WriteLine("Start reading.");
+
+            while (true)
+            {
+                string? line = input.ReadLine();
+                if (line is null)
+                {
+                    Console.WriteLine("Parsing complete.");
+                    return addresses;
+                }
+
+                var chunks = line.Split(',')!;
+
+                if (chunks.Length is 6)
+                {
+                    var name = chunks[0];
+                    var surname = chunks[1];
+                    var street = chunks[2];
+                    var city = chunks[3];
+                    var province = chunks[4];
+                    var zip = chunks[5];
+
+                    var address = new Address(name, surname, street, city, province, zip);
+                    addresses.Add(address);
+
+                    Console.WriteLine("Object successfully parsed.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid format, discarding object.");
+                }
+            }
+        }
+    }
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,8 @@
+﻿using ListaIndirizzi;
+
+var addresses = Parser.Read();
+
+foreach (var address in addresses)
+{
+    Console.WriteLine(address);
+}
