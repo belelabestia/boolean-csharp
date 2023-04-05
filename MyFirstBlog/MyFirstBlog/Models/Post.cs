c18a2d331fb57f23110e02050e843186b4ebb90b
Marco Bellezza <jasone.mb@outlook.com>
2023-04-05 13:03:40 +0200

    update blog example

---
+++ MyFirstBlog/MyFirstBlog/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MyFirstBlog.Models
+{
+    public class Post
+    {
+        public Post(string imageSrc, string title, string description)
+        {
+            ImageSrc = imageSrc;
+            Title = title;
+            Description = description;
+        }
+
+        public string ImageSrc { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
