58f7492433471349a459368c659693e0c9d87635
Marco Bellezza <jasone.mb@outlook.com>
2023-04-06 13:05:56 +0200

    random student picker: support commented lines

--- RandomStudent/Program.cs
+++ RandomStudent/Program.cs
@@ -1,6 +1,10 @@
 ﻿using static System.Console;
 
-var students = File.ReadAllLines("data.txt");
+var students = File
+    .ReadAllLines("data.txt")
+    .Where(l => !l.StartsWith('#'))
+    .ToArray();
+
 var picker = new Picker(students);
 
 WriteLine(picker.Pick());
