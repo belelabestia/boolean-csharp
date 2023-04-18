fff18c5cf8323f7afc525eb879ec5d0f45a6a419
Marco Bellezza <jasone.mb@outlook.com>
2023-04-18 13:37:39 +0200

    add nullables demo

---
+++ Nullables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+var a = new NumberWrapper();
+
+a.Name = null;
+
+class NumberWrapper
+{
+    public Nullable<int> NullableValue { get; set; }
+    public List<int> Reference { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
